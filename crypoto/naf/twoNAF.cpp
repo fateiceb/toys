3904601bfdcf98a6ba751a923bbe22c42e95c271
fateiceb <993661862@qq.com>
2021-04-30 12:15:59 +0800

    add comments on twoNaf

--- crypoto/naf/twoNAF.cpp
+++ crypoto/naf/twoNAF.cpp
@@ -8,31 +8,31 @@ int main(){
     cout << "please input number:"<<endl;
     cin >> n;
     while(n > 0){
-        int t = n &0x3;
+        int t = n &0x3;     //取最后两位，有可能的值0,1,2,3
         switch (t) {
             case 0: 
-                k[i] = 0;
-                k[i+1] = 0;
-                i=i+2;
-                n = n >> 2;
+                k[i] = 0;       //k[i],k[i+1]一次生成两位
+                k[i+1] = 0;     
+                i=i+2;          //i后移两位
+                n = n >> 2; //  n/4
                 break;
             case 1: 
                 k[i] = 1;
                 k[i+1] = 0;
                 i=i+2;
-                n = n >> 2;
+                n = n >> 2; //  n/4
                 break;
             case 2: 
                 k[i] = 0;
                 i=i+1;
-                n = n >> 1;
+                n = n >> 1;      // n/2,n除以2
                 break;
             case 3: 
                 k[i] = -1;
                 k[i+1] = 0;
                 i=i+2;
                 n = n+1;
-                n = n >> 2;
+                n = n >> 2;   //    n/4
                 break;
         }
     }
@@ -41,7 +41,7 @@ int main(){
     cout<<k[j]<<" ";
   cout<<endl;
   
-  for(int j=i-2;j>0;j--)     
+  for(int j=i-2;j>0;j--)     //NAF标量乘算法的实现 
   {
    q=2*q;
    if(k[j-1]>0)
